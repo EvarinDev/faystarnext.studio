6f81b093dd8c3408d89295c303a0016ed7d9443c
FAYシ <phasordev@gmail.com>
2024-04-08 20:10:07 +0700

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,34 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    devtools: { enabled: true },
+    app: {
+      pageTransition: { name: "page", mode: "out-in" },
+      head: {
+        htmlAttrs: {
+          lang: "en-US",
+        },
+        title: '🚀 | FAYStarNext',
+        
+        link: [
+          { rel: 'icon', href: 'https://cdn.rlss.in.th/profile/vtube/7255b71064ecf98bcee2ac98e679f498.png' }
+        ],
+  
+      }
+    },
+    css: ["/public/assets/css/tailwind.css"],
+    routeRules: {
+      // prerender index route by default
+      '/': { prerender: true, cors: true },
+      '/*': { cors: true },
+    },
+    modules: ["@nuxtjs/tailwindcss", '@nuxt/image', "@nuxtjs/sitemap", "nuxt-gtag", "@nuxtjs/robots"],
+    gtag: {
+      id: "",
+    },
+    postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    },
+  })
---
+++ tailwind.config.ts
@@ -0,0 +1,62 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./node_modules/flowbite/**/*.{js,ts}"
+  ],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'body': [
+          'Inter',
+          'ui-sans-serif',
+          'system-ui',
+          '-apple-system',
+          'system-ui',
+          'Segoe UI',
+          'Roboto',
+          'Helvetica Neue',
+          'Arial',
+          'Noto Sans',
+          'sans-serif',
+          'Apple Color Emoji',
+          'Segoe UI Emoji',
+          'Segoe UI Symbol',
+          'Noto Color Emoji'
+        ],
+        'sans': [
+          'Inter',
+          'ui-sans-serif',
+          'system-ui',
+          '-apple-system',
+          'system-ui',
+          'Segoe UI',
+          'Roboto',
+          'Helvetica Neue',
+          'Arial',
+          'Noto Sans',
+          'sans-serif',
+          'Apple Color Emoji',
+          'Segoe UI Emoji',
+          'Segoe UI Symbol',
+          'Noto Color Emoji'
+        ]
+      }
+    },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ['dark'],
+      borderColor: ['dark'],
+      textColor: ['dark'],
+    },
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
