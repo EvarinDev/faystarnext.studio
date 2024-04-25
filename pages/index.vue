21609cdb847e038d5ba793ce8805811f73ccf36a
FAYシ <phasordev@gmail.com>
2024-04-25 13:03:07 +0700

    Update Discord link in index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -21,7 +21,7 @@
                 clip-rule="evenodd" />
             </svg>
           </a>
-          <a href="https://discord.com/users/395597735174340611" aria-label="Discord">
+          <a href="https://discord.com/invite/ekQq9t3UkK" aria-label="Discord">
             <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
               width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
               <path
