d04e26c7ac0b2ee6c24dea2008078993f9f2714a
FAYシ <phasordev@gmail.com>
2024-04-28 16:43:29 +0700

    Add metadata to _app.tsx and index.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,26 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import * as React from "react";
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '🚀 | FAYStarNext',
+  description: '🚀 | FAYStarNext',
+  keywords: 'FAYStarNext',
+  openGraph: {
+    images: ["https://cdn.rlss.in.th/assets/img/BackGround.webp"],
+    type: 'website',
+    title: '🚀 | FAYStarNext',
+    description: 'Hi 👋 My name is FAY',
+    url: 'https://faystarnext.studio',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: '🚀 | FAYStarNext',
+    description: '🚀 | FAYStarNext',
+    images: 'https://cdn.rlss.in.th/assets/img/BackGround.webp',
+  }
+};
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
