d2f5d0c82a7e50e0afb07007d124ec7d87c0eade
FAYシ <phasordev@gmail.com>
2024-04-28 16:23:36 +0700

    Add meta tags for SEO and social media sharing

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,20 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="title" content="🚀 | FAYStarNext" />
+        <meta name="description" content="🚀 | FAYStarNext" />
+        <meta name="keywords" content="FAYStarNext" />
+        <meta property="og:image" content="https://cdn.rlss.in.th/assets/img/BackGround.webp" />
+        <meta property="og:url" content="https://faystarnext.studio" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="🚀 | FAYStarNext" />
+        <meta property="og:description" content="Hi 👋 My name is FAY" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="🚀 | FAYStarNext" />
+        <meta name="twitter:description" content="🚀 | FAYStarNext" />
+        <meta name="twitter:image" content="https://cdn.rlss.in.th/assets/img/BackGround.webp" />
+      </Head>
       <body>
         <Main />
         <NextScript />
