fcf02f42fbecb5685771eb0da1779a89569f3ace
FAYシ <103030954+FAYStarNext@users.noreply.github.com>
2024-07-07 14:37:51 +0700

    chore: Enable unoptimized image loading and update output configuration

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,9 @@ import withTM from 'next-transpile-modules';
 
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
